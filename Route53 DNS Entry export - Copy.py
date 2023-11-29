60cd3bb8c5731cf18f35697f0eaf8d1bb22accec
storablekamal <147033662+storablekamal@users.noreply.github.com>
2023-11-29 14:58:37 +0530

    Update Route53 DNS Entry export - Copy.py

--- Route53 DNS Entry export - Copy.py
+++ Route53 DNS Entry export - Copy.py
@@ -2,9 +2,24 @@ import boto3
 import csv
 import json
 
-input_json = 'account_ids.json' 
+input_json = "account_ids.json"
 output_csv = 'dns_entries.csv'
 
+def process_records(records, csv_writer, account_id, hosted_zone_name):
+    for record in records['ResourceRecordSets']:
+        name = record['Name']
+        record_type = record['Type']
+
+        if record_type in ['A', 'CNAME']:
+            value = None
+
+            if 'ResourceRecords' in record:
+                value = record['ResourceRecords'][0]['Value']
+            elif 'AliasTarget' in record:
+                value = record['AliasTarget']['DNSName']
+            
+            csv_writer.writerow(["'" + str(account_id), hosted_zone_name, name, record_type, value])
+            
 # Get a list of account IDs in the organization
 with open(input_json, 'r') as json_file:
     account_ids = json.load(json_file)
@@ -12,7 +27,7 @@ with open(input_json, 'r') as json_file:
 # Loop through each account and list DNS entries for each hosted zone 
 with open(output_csv, mode='w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Account ID', 'Hosted Zone', 'Name', 'TTL', 'Type', 'Value'])
+    csv_writer.writerow(['Account ID', 'Hosted Zone Name', 'Name', 'Type', 'Value'])
 
     for account_id in account_ids:
         print (f"Processing Account ID: {account_id}")
@@ -27,19 +42,15 @@ with open(output_csv, mode='w', newline='') as csv_file:
         #Loop Through each hosted zone and list DNS Entries
         for hosted_zone in hosted_zones:
             hosted_zone_id = hosted_zone['Id'].split('/')[-1]
-
+            hosted_zone_name = hosted_zone['Name']
             # List DNS entries for each hosted zone
             resource_record_sets = route53_client.list_resource_record_sets(
                 HostedZoneId=hosted_zone_id
-            )['ResourceRecordSets']
-
+            )
+            
             # Convert DNS entries to CSV
-            for record_set in resource_record_sets: 
-                name = record_set['Name']
-                record_type = record_set['Type']
-                # ttl = record_set['TTL']
-                values = [value['Value'] for value in record_set.get('ResourceRecords', [])]
-
-                csv_writer.writerow([account_id, hosted_zone['Name'], record_type, name, ".".join(values)])
-
+            if (resource_record_sets):
+            #  for record_set in resource_record_sets: 
+                process_records(resource_record_sets, csv_writer, account_id, hosted_zone_name)
+                  
 print("CSV file exported to: [output_csv}")
